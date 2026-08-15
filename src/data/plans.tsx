89ce106c166b78781b31170b6a0eaabc9b6f9e1c
Anastasiya Haiduk <anastasiagajduk24@gmail.com>
2026-08-15 14:28:58 +0200

    feat: add more features to pricing plans

--- src/data/plans.tsx
+++ src/data/plans.tsx
@@ -1,22 +1,43 @@
-import type {PricingPlan} from "../types";
+import type { PricingPlan } from "../types";
 
 export const plans: PricingPlan[] = [
   {
     title: "Basic",
     monthlyPrice: 10.99,
     annualDiscount: 10,
-    features: [{ name: "analytics", isIncluded: false }],
+    features: [
+      { name: "analytics", isIncluded: true },
+      { name: "Unlimited Projects", isIncluded: true },
+      { name: "Priority Support", isIncluded: false },
+      { name: "Custom Domain", isIncluded: false },
+      { name: "Team Collaboration", isIncluded: false },
+      { name: "API Access", isIncluded: false },
+    ],
   },
   {
     title: "Pro",
     monthlyPrice: 40.99,
     annualDiscount: 10,
-    features: [{ name: "analytics", isIncluded: true }],
+    features: [
+      { name: "analytics", isIncluded: true },
+      { name: "Unlimited Projects", isIncluded: true },
+      { name: "Priority Support", isIncluded: true },
+      { name: "Custom Domain", isIncluded: true },
+      { name: "Team Collaboration", isIncluded: false },
+      { name: "API Access", isIncluded: false },
+    ],
   },
   {
     title: "Enterprise",
     monthlyPrice: 100.99,
     annualDiscount: 10,
-    features: [{ name: "analytics", isIncluded: true }],
-  }
+    features: [
+      { name: "analytics", isIncluded: true },
+      { name: "Unlimited Projects", isIncluded: true },
+      { name: "Priority Support", isIncluded: true },
+      { name: "Custom Domain", isIncluded: true },
+      { name: "Team Collaboration", isIncluded: true },
+      { name: "API Access", isIncluded: true },
+    ],
+  },
 ];
